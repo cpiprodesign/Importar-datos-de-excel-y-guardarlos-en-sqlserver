20db60e1b9392788382c603b3acaf54aff63fcfa
Cristian <Cristian@CPIPRODESIGN>
2020-06-11 12:43:35 -0500

    Add

---
+++ Importar datos/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Importar_datos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataDetalles = new System.Windows.Forms.DataGridView();
+            this.BtnImportarDatos = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtcadena = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataDetalles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataDetalles
+            // 
+            this.DataDetalles.AllowUserToAddRows = false;
+            this.DataDetalles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataDetalles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataDetalles.Location = new System.Drawing.Point(12, 43);
+            this.DataDetalles.Name = "DataDetalles";
+            this.DataDetalles.Size = new System.Drawing.Size(620, 395);
+            this.DataDetalles.TabIndex = 0;
+            // 
+            // BtnImportarDatos
+            // 
+            this.BtnImportarDatos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnImportarDatos.BackColor = System.Drawing.Color.DeepPink;
+            this.BtnImportarDatos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnImportarDatos.Location = new System.Drawing.Point(638, 12);
+            this.BtnImportarDatos.Name = "BtnImportarDatos";
+            this.BtnImportarDatos.Size = new System.Drawing.Size(150, 50);
+            this.BtnImportarDatos.TabIndex = 1;
+            this.BtnImportarDatos.Text = "Importar Datos";
+            this.BtnImportarDatos.UseVisualStyleBackColor = false;
+            this.BtnImportarDatos.Click += new System.EventHandler(this.BtnImportarDatos_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepPink;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(638, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Registrar Datos";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtcadena
+            // 
+            this.txtcadena.Location = new System.Drawing.Point(188, 15);
+            this.txtcadena.Name = "txtcadena";
+            this.txtcadena.Size = new System.Drawing.Size(408, 20);
+            this.txtcadena.TabIndex = 3;
+            this.txtcadena.Text = "Data Source=cpiprodesign;Initial Catalog=CM_FARMA;Integrated Security=True";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cadena de conexion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtcadena);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnImportarDatos);
+            this.Controls.Add(this.DataDetalles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMPORTAR DATOS DE EXCEL AL SISTEMA";
+            ((System.ComponentModel.ISupportInitialize)(this.DataDetalles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataDetalles;
+        private System.Windows.Forms.Button BtnImportarDatos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtcadena;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Importar datos/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Importar_datos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataView ImportarDatos(string nombrearchivo)
+        {
+            //UTILIZAMOS 12.0 DEPENDIENDO DE LA VERSION DEL EXCEL, EN CASO DE QUE LA VERSIÓN QUE TIENES ES INFERIOR AL DEL 2013, CAMBIAR POR A EXCEL 8.0 Y EN VEZ DE
+            //ACE.OLEDB.12.0 UTILIZAR LO SIGUIENTE (Jet.Oledb.4.0)
+            string conexion = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 12.0;'", nombrearchivo);
+
+            OleDbConnection conector = new OleDbConnection(conexion);
+
+            conector.Open();
+
+            //DEPENDIENDO DEL NOMBRE QUE TIENE LA PESTAÑA EN TU ARCHIVO EXCEL COLOCAR DENTRO DE LOS []
+            OleDbCommand consulta = new OleDbCommand("select * from [Productos$]", conector);
+
+            OleDbDataAdapter adaptador = new OleDbDataAdapter
+            {
+                SelectCommand = consulta
+            };
+
+            DataSet ds = new DataSet();
+
+            adaptador.Fill(ds);
+
+            conector.Close();
+
+            return ds.Tables[0].DefaultView;
+        }
+
+        private void BtnImportarDatos_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                //DE ESTA MANERA FILTRAMOS TODOS LOS ARCHIVOS EXCEL EN EL NAVEGADOR DE ARCHIVOS
+                Filter = "Excel | *.xls;*.xlsx;",
+                
+                //AQUÍ INDICAMOS QUE NOMBRE TENDRÁ EL NAVEGADOR DE ARCHIVOS COMO TITULO
+                Title = "Seleccionar Archivo"
+            };
+
+            //EN CASO DE SELECCIONAR EL ARCHIVO, ENTONCES PROCEDEMOS A ABRIR EL ARCHIVO CORRESPONDIENTE
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                DataDetalles.DataSource = ImportarDatos(openFileDialog.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cadena;
+            cadena = txtcadena.Text;
+           // SqlConnection cn;
+            
+            //lblfecha.Text = DateTime.Now.ToString("dd/mm/yyyy");
+            //lbltiempo.Text = DateTime.Now.ToString("HH:mm:ss");
+            SqlConnection cn = new SqlConnection(cadena);
+           // cn = new SqlConnection(cadena);//demo conexion
+
+
+            try
+            {
+                if ((DataDetalles.Rows.Count == 0))
+                {
+                    return;
+                }
+
+                string query = "insert into Productos values (@Codigo_Barras, @Descripcion, @UsoTerapeutico, @SustanciaActiva, @Precio_compra, @Precio_venta, @Fecha_vencimiento, @stock, @Ubicacion, @Lote, @cod_lab, @cod_cate, @cod_prov, @cod_pre, @IGV_incluido, @Descuento, @Estado)";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                foreach (DataGridViewRow row in DataDetalles.Rows)
+                {
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@Codigo_Barras", Convert.ToString(row.Cells["Codigo_Barras"].Value));
+                    cmd.Parameters.AddWithValue("@Descripcion", Convert.ToString(row.Cells["Descripcion"].Value));
+                    //aqui esta el error al pasar la fecha y la hora no reconoce el formato
+                    cmd.Parameters.AddWithValue("@UsoTerapeutico", Convert.ToString(row.Cells["UsoTerapeutico"].Value));
+                    //aqui esta el error al pasar la fecha y la hora no reconoce el formato
+                    cmd.Parameters.AddWithValue("@SustanciaActiva", Convert.ToString(row.Cells["SustanciaActiva"].Value));
+                    cmd.Parameters.AddWithValue("@Precio_compra", Convert.ToDecimal(row.Cells["Precio_compra"].Value));
+                    cmd.Parameters.AddWithValue("@Precio_venta", Convert.ToDecimal(row.Cells["Precio_venta"].Value));
+
+                    cmd.Parameters.AddWithValue("@Fecha_vencimiento", Convert.ToDateTime(row.Cells["Fecha_vencimiento"].Value));
+                    cmd.Parameters.AddWithValue("@stock", Convert.ToInt64(row.Cells["stock"].Value));
+                    cmd.Parameters.AddWithValue("@Ubicacion", Convert.ToString(row.Cells["Ubicacion"].Value));
+                    cmd.Parameters.AddWithValue("@Lote", Convert.ToString(row.Cells["Lote"].Value));
+                    cmd.Parameters.AddWithValue("@cod_lab", Convert.ToInt64(row.Cells["Laboratorio"].Value));
+                    cmd.Parameters.AddWithValue("@cod_cate", Convert.ToInt64(row.Cells["Categoria"].Value));
+                    cmd.Parameters.AddWithValue("@cod_prov", Convert.ToInt64(row.Cells["Proveedor"].Value));
+                    cmd.Parameters.AddWithValue("@cod_pre", Convert.ToInt64(row.Cells["Presentacion"].Value));
+                    cmd.Parameters.AddWithValue("@IGV_incluido", Convert.ToString(row.Cells["IGV_incluido"].Value));
+                    cmd.Parameters.AddWithValue("@Descuento", Convert.ToString(row.Cells["Descuento"].Value));
+                    cmd.Parameters.AddWithValue("@Estado", Convert.ToString(row.Cells["Estado"].Value));
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    
+                }
+                MessageBox.Show("Datos ingresados correctamente");
+
+            }
+            catch (Exception f)
+            {
+
+                MessageBox.Show(f.Message);
+            }
+        }
+    }
+}
